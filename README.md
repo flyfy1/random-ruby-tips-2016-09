d52d3a1deffdec16359c6a8c4c78da639d2a6144
Song Yangyu <flyfy1@gmail.com>
2016-09-20 18:31:37 +0800

    update for one more example of class_variable

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 2. `each_with_object.rb`
 3. `caller.rb`
 4. `eval-class-variable.rb`
+  - `eval-class-variable.rb`
+  - `evil_class_var_extend.rb`
+  - `evil_class_var_include.rb`
 5. jeweler, and a meta way of getting methods
 
--- operator-symbol.rb
+++ operator-symbol.rb
@@ -5,10 +5,16 @@ e.explicit('1 + 2')
 e.explicit('1 .+ 2')
 e.explicit('1.+(2)')
 
+puts
+
 e.explicit('h = {}')
 e.explicit('h["a"] = 1; h')
 e.explicit('h.send :[]=, "b", 2; h')
 
+puts
+
 e.explicit('def m; end')
+
+puts
 
 e.explicit('[1,2,3,4].reduce(:+)')
